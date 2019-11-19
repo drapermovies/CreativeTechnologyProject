de045495a21bb7714dc58868c9a78eae5bf58867
Joel Draper <jepdraper@gmail.com>
2019-11-19 15:40:40 +0000

    Added sample code
    Implemented code that spawns Entities
    Tried implementing rotation code, however this was problematic

---
+++ Assets/Code/Spawner.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities; //ECS Library 
+
+public struct Spawner : IComponentData
+{
+    public int count_x;
+    public int count_y;
+    public Entity prefab;
+}
---
+++ Assets/Code/SpawnerAuthoring.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+[RequiresEntityConversion]
+public class SpawnerAuthoring : MonoBehaviour, IDeclareReferencedPrefabs, IConvertGameObjectToEntity
+{
+    public GameObject prefab;
+    public int count_x = 0;
+    public int count_y = 0;
+
+    public void DeclareReferencedPrefabs(List<GameObject> referenced_prefabs)
+    {
+        referenced_prefabs.Add(prefab);
+    }
+
+    public void Convert(Entity entity, EntityManager manager, 
+                        GameObjectConversionSystem conversion_system)
+    {
+        Spawner spawner_data = new Spawner
+        {
+            prefab = conversion_system.GetPrimaryEntity(prefab),
+            count_x = count_x,
+            count_y = count_y
+        };
+
+        manager.AddComponentData(entity, spawner_data);
+    }
+}
